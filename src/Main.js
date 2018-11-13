c7758328e59984e701946cd3cd31eab113a92240
John Flynn <jflynnpics@gmail.com>
2018-11-13 11:59:32 -0500

    React in class work

---
+++ src/Main.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Article from './Article'
+class Main extends Component {
+  render() {
+    return (
+      <main>
+        <Article title="Why Gavin are you talking about food before lunch" />
+        <Article title="I want some food" />
+        <Article title="gimme some tacos" />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+      </main>
+    )
+  }
+}
+export default Main
